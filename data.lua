120fdef1cea48d182b51d1908943d199db385717
Tomáš Chmelík <tomas.chmelik2@gmail.com>
2024-01-14 08:18:25 +0100

    First version

---
+++ data.lua
@@ -0,0 +1,12 @@
+MergingChests.create_mergeable_chest(
+    {
+        chest_name = 'cellulose-fiber-chest'
+    },
+    MergingChests.steel_chest_segments
+)
+MergingChests.create_mergeable_chest(
+    {
+        chest_name = 'paper-chest'
+    },
+    MergingChests.steel_chest_segments
+)
---
+++ settings.lua
@@ -0,0 +1,2 @@
+MergingChests.create_mergeable_chest_setting('cellulose-fiber-chest', { default_value = 'none', order = '01' })
+MergingChests.create_mergeable_chest_setting('paper-chest', { default_value = 'none', order = '02' })
